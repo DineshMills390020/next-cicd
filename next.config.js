90ed9aad599fc83f1f667e859a5242cd9feaf8e2
Dineshmills390020 <viken@dineshmills.com>
2023-10-22 15:38:58 +0530

    change in next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    reactStrictMode: true,
+    output: 'export',
+    trailingSlash: true,
+    images: {
+      domains: ['localhost'],
+      unoptimized: true
+    },
+  }
+  
+  module.exports = nextConfig
