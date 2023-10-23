9dce14e4bd5779cdfec7e9836d7658a181bc95c3
Dineshmills390020 <viken@dineshmills.com>
2023-10-23 11:09:09 +0530

    chnage in next config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    output: 'export',
-    trailingSlash: true,
-    images: {
-      domains: ['localhost','dineshmills390020.github.io'],
-      unoptimized: true
-    },
-  }
-  
-  module.exports = nextConfig
+  reactStrictMode: true,
+  output: 'export',
+  basePath: '/next-cicd',
+  assetPrefix: '/next-cicd',
+  trailingSlash: true,
+  images: {
+    domains: ['localhost', 'dineshmills390020.github.io'],
+    unoptimized: true
+  },
+}
+
+module.exports = nextConfig
