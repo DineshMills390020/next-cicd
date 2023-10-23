e99fd117b2099ac022ae9103720a24ac592bfef3
Dineshmills390020 <viken@dineshmills.com>
2023-10-23 11:19:02 +0530

    change for image show on live

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  //output: 'export',
+  output: 'export',
   basePath: '/next-cicd',
   assetPrefix: '/next-cicd',
   trailingSlash: true,
